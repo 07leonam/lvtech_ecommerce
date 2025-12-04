41ce9c906a85111f5beea6307a7362fadb71627c
Leonam Veggi <87779062+07leonam@users.noreply.github.com>
2025-12-04 12:46:25 -0300

    Fix formatting in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/lvtech_ecommerce/'
+  base: '/lvtech_ecommerce/',
   plugins: [
     vue(),
     vueDevTools(),
