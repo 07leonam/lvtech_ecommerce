df451bb8e8ff34e8ae92fb164993bc0cb8c2ba73
Leonam Veggi <87779062+07leonam@users.noreply.github.com>
2025-12-04 12:57:52 -0300

    Add DB_PORT to configuration with default value

--- backend/config.js
+++ backend/config.js
@@ -3,4 +3,5 @@ module.exports = {
     DB_USER: process.env.DB_USER || 'root',
     DB_PASSWORD: process.env.DB_PASSWORD || '',
     DB_NAME: process.env.DB_NAME || 'ecommerce_iphone',
+    DB_PORT: process.env.DB_PORT || 3306 // <--- Adicione isso
 };
